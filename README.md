39dcc82f05743d1d7edd788e8d42ec26fc73c5c1
Riccardo Balbo <riccardo@forkforge.net>
2016-03-26 21:14:49 +0100

    Moved to gradle

--- README.md
+++ README.md
@@ -1,31 +1,8 @@
 # SVG Loader
-An asset loader for jmonkeyengine that loads svg images.
+An asset loader for jmonkeyengine that loads svg images with apache batik.
 
-##Download 
-Latest release: [Version 1.0](https://bintray.com/artifact/download/riccardo/jme3-plugins/net/forkforge/jme3_plugins/svg_loader/1.0/svg_loader-1.0.jar)
-
-Dependencies: [Apache Batik](http://apache.panu.it/xmlgraphics/batik/binaries/)
-
-##Maven
-```
-<repository>
-  <snapshots>
-    <enabled>false</enabled>
-  </snapshots>
-  <id>bintray-riccardo-jme3-plugins</id>
-  <name>bintray-riccardo-jme3-plugins</name>
-  <url>http://dl.bintray.com/riccardo/jme3-plugins</url>
-</repository>
-```
-
-```
-<dependency>
-  <groupId>net.forkforge.jme3_plugins</groupId>
-  <artifactId>svg_loader</artifactId>
-  <version>1.0</version>
-  <type>jar</type>
-</dependency>
-```
+##Gradle
+[https://jitpack.io/#riccardobl/jme3-plugin-svg-loader](https://jitpack.io/#riccardobl/jme3-plugin-svg-loader)
 
 ##Usage
 ```
--- src/test/java/SVGLoaderTest.java
+++ src/test/java/SVGLoaderTest.java
@@ -1,36 +1,10 @@
-/**
- * @name SVGLoader
- * @version 1.0
- * @author Riccardo Balbo
- * @license
- * 	This is free and unencumbered software released into the public domain.
- * 	Anyone is free to copy, modify, publish, use, compile, sell, or
- * 	distribute this software, either in source code form or as a compiled
- * 	binary, for any purpose, commercial or non-commercial, and by any
- * 	means.
- * 	In jurisdictions that recognize copyright laws, the author or authors
- * 	of this software dedicate any and all copyright interest in the
- * 	software to the public domain. We make this dedication for the benefit
- * 	of the public at large and to the detriment of our heirs and
- * 	successors. We intend this dedication to be an overt act of
- * 	relinquishment in perpetuity of all present and future rights to this
- * 	software under copyright law.
- * 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * 	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * 	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * 	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * 	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * 	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * 	OTHER DEALINGS IN THE SOFTWARE.
- * 	For more information, please refer to <http://unlicense.org>
- */
-
 import net.forkforge.jme3_plugins.svg_loader.SVGImage;
 import net.forkforge.jme3_plugins.svg_loader.SVGLoader;
 
 import org.apache.batik.transcoder.TranscoderException;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector2f;
 import com.jme3.texture.Image;
 import com.jme3.texture.Texture2D;
@@ -44,7 +18,7 @@ public class SVGLoaderTest extends SimpleApplication{
 	@Override
 	public void simpleInitApp() {
 		SVGLoader.init(assetManager);
-		SVGImage svg=(SVGImage)assetManager.loadAsset("awesome_tiger.svg");
+		SVGImage svg=(SVGImage)assetManager.loadAsset("publicdomain.loopyasiandragonframe.svg");
 
 		try{
 			addInstance(svg,64,0,0);
@@ -59,6 +33,7 @@ public class SVGLoaderTest extends SimpleApplication{
 		}catch(Throwable e){
 			e.printStackTrace();
 		}
+		this.getViewPort().setBackgroundColor(ColorRGBA.White);
 		
 	}
 	
